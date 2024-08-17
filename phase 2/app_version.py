28f92f1b05290e12762108dff61f92224e0ec0f2
hassannader2040 <79514700+hasannader2040@users.noreply.github.com>
2024-08-17 21:01:10 +0300

    first commit

---
+++ phase 2/app_version.py
@@ -0,0 +1,15 @@
+import sys
+
+def print_app_version_and_exit():
+    # Define the current version of the application
+    app_version = "v0.0.1"
+    
+    # Print the current version
+    print(f"Current application version: {app_version}")
+    
+    # Exit the application
+    sys.exit(0)
+
+# Example of calling the function
+if __name__ == "__main__":
+    print_app_version_and_exit()
