aec6f971c4d7b59e6c24da7bb79f491390ad55c4
hasannader2040 <hassannader20400@gmail.com>
2024-10-15 21:06:32 +0300

    feat(fix): corrcting the erorr of the sonar and adding some files for manfest kubernetes

--- test/integration/test_api.py
+++ test/integration/test_api.py
@@ -4,3 +4,4 @@ def test_get_temperature_api():
     response = requests.get("http://127.0.0.1:5000/temperature")
     assert response.status_code == 200
     assert "temperature" in response.json()
+
